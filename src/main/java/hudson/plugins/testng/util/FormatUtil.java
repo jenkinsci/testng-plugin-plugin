2e03c9b9719081f3c0d2562836985d1db8aa690c
nalin_makar <nalin_makar@dev.java.net>
2010-09-08 21:10:15 +0000

    Fixed: Better display of content on the test method result page
    1. Display exception related info, only if it exists
    2. Print stack trace properly for better readability
    3. Fix title
    4. Add required methods in FormatUtil. Also changed FormatUtil's time format method to show 0 min and 0 sec if applicable
    5. Description for TestNG tests are not editable

--- src/main/java/hudson/plugins/testng/util/FormatUtil.java
+++ src/main/java/hudson/plugins/testng/util/FormatUtil.java
@@ -4,26 +4,44 @@ import java.util.concurrent.TimeUnit;
 
 public class FormatUtil {
 
-
+   /**
+    * Formats the time into mill
+    * @param duration
+    * @return
+    */
    public static String formatTimeInMilliSeconds(long duration) {
       try {
-         String durationInString = "";
+         StringBuffer durationInString = new StringBuffer("");
          long minutes = TimeUnit.MILLISECONDS.toMinutes(duration);
          long seconds = TimeUnit.MILLISECONDS.toSeconds(duration - minutes * 60 * 1000);
          long milliseconds = duration - (minutes * 60 * 1000) - seconds * 1000;
-         if (minutes > 0) {
-            durationInString += minutes + " mins ";
-         }
-         if (seconds > 0) {
-            durationInString += seconds + " sec ";
-         }
-         if (milliseconds >= 0) {
-            durationInString += milliseconds + " msec";
-         }
-         return durationInString;
+         durationInString.append(minutes).append(" min ");
+         durationInString.append(seconds).append(" sec ");
+         durationInString.append(milliseconds).append(" msec");
+         return durationInString.toString();
       } catch (Exception e) {
          e.printStackTrace();
          return "-1";
       }
+   }
+
+   /**
+    * Replaces newline characters in string with <code>&lt;br/&gt;</code> to retain
+    * the newlines when the string is displayed in HTML
+    *
+    * @param str
+    * @return
+    */
+   public static String replaceNewLineWithBR(String str) {
+      return str == null ? "" : str.replace("\n", "<br/>");
+   }
+
+   /**
+    * Formats the stack trace for easier readability
+    * @param stackTrace
+    * @return
+    */
+   public static String formatStackTraceForHTML(String stackTrace) {
+      return replaceNewLineWithBR(stackTrace);
    }
 }
