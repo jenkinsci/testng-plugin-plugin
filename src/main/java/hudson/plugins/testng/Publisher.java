2535fd7467844ad7657d880875de32b0ebd81b52
nullin <nalin.makar@gmail.com>
2011-05-29 01:23:03 -0700

    Remove AbstractPublisherImpl, BuildProxy, BuildProxyCallableHelper and GhostWriter

--- src/main/java/hudson/plugins/testng/Publisher.java
+++ src/main/java/hudson/plugins/testng/Publisher.java
@@ -1,12 +1,24 @@
 package hudson.plugins.testng;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
 import hudson.Extension;
+import hudson.FilePath;
+import hudson.Launcher;
+import hudson.model.AbstractBuild;
 import hudson.model.Action;
 import hudson.model.AbstractProject;
-import hudson.plugins.helpers.AbstractPublisherImpl;
-import hudson.plugins.helpers.Ghostwriter;
+import hudson.model.BuildListener;
+import hudson.model.Result;
+import hudson.plugins.testng.parser.ResultsParser;
+import hudson.plugins.testng.results.TestResults;
 import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.BuildStepMonitor;
+import hudson.tasks.Recorder;
 import net.sf.json.JSONObject;
 
 import org.kohsuke.stapler.DataBoundConstructor;
@@ -16,7 +28,7 @@ import org.kohsuke.stapler.StaplerRequest;
  * This class defines a @Publisher and @Extension
  *
  */
-public class Publisher extends AbstractPublisherImpl {
+public class Publisher extends Recorder {
 
    private final String reportFilenamePattern;
    private final boolean isRelativePath;
@@ -72,9 +84,61 @@ public class Publisher extends AbstractPublisherImpl {
       return new ProjectIndividualReport(project, escapeTestDescp, escapeExceptionMsg);
    }
 
-   protected Ghostwriter newGhostwriter() {
-      //return new GhostWriter(reportFilenamePattern, targets);
-      return new GhostWriter(reportFilenamePattern, isRelativePath);
+   /**
+    * {@inheritDoc}
+    */
+   @Override
+   public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, final BuildListener listener)
+         throws InterruptedException, IOException {
+      Collection<TestResults> results = null;
+      Set<String> parsedFiles = new HashSet<String>();
+      if (!isRelativePath) {
+         //TODO : fix this code to handle relative and absolute path together
+         //instead of branching here
+         FilePath[] paths = build.getWorkspace().list(reportFilenamePattern);
+
+         //loop through all the files and get the results
+         for (FilePath path : paths) {
+            final String pathStr = path.getRemote();
+            if (!parsedFiles.contains(pathStr)) {
+               parsedFiles.add(pathStr);
+               ResultsParser parser = new ResultsParser(listener.getLogger());
+               Collection<TestResults> result = parser.parse(new File(pathStr));
+               if (results == null) {
+                  results = result;
+               } else {
+                  results.addAll(result);
+               }
+            }
+         }
+      } else {
+         String executionRootDirRemotePath = build.getWorkspace().getRemote();
+         String testngResultXmlRelativePath = reportFilenamePattern;
+         String testngResultXmlRemotePath = executionRootDirRemotePath + "/" + testngResultXmlRelativePath;
+         ResultsParser parser = new ResultsParser(listener.getLogger());
+         results = parser.parse(new File(testngResultXmlRemotePath));
+      }
+
+      if (results != null) {
+         //create an individual report for all of the results and add it to the build
+         BuildIndividualReport action = new BuildIndividualReport(results);
+         action.setBuild(build);
+         build.getActions().add(action);
+         TestResults r = TestResults.total(true, results);
+         if (r.getFailedConfigurationMethodsCount() > 0 || r.getSkippedConfigurationMethodsCount() > 0 ||
+               r.getFailedTestCount() > 0 || r.getSkippedTestCount() > 0) {
+            build.setResult(Result.UNSTABLE);
+         }
+      }
+      return true;
+   }
+
+   /**
+    * {@inheritDoc}
+    */
+   @Override
+   public boolean prebuild(AbstractBuild<?, ?> build, BuildListener listener) {
+      return true;
    }
 
    public static final class DescriptorImpl extends BuildStepDescriptor<hudson.tasks.Publisher> {
