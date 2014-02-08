a960d5ab7b8a8a2901d7f48fa98a7c4bcb5b457c
Nalin Makar <nullin@nullin.com>
2014-02-08 10:49:03 -0800

    update debug.sh

--- debug.sh
+++ debug.sh
@@ -1,2 +1,2 @@
-export MAVEN_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,address=8000,suspend=n"
-mvn hpi:run
+export MAVEN_OPTS="-XX:MaxPermSize=256m -Xmx1g -Xdebug -Xrunjdwp:transport=dt_socket,server=y,address=8000,suspend=n"
+mvn hpi:run -Djava.awt.headless=true 
