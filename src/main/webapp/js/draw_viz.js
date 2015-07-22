2eabd327712a800210816c086ff59bea21c92280
Saksa <eric.saksa@ES200873ML.i3domain.inin.com>
2015-07-22 13:03:39 -0400

    beautifying the graph

--- src/main/webapp/js/draw_viz.js
+++ src/main/webapp/js/draw_viz.js
@@ -1,23 +1,36 @@
 function resultsGraph(id) {
 
-    var mockdata = [
-        ['fail', 3, 4, 4, 6, 4, 5, 5, 5, 5, 2, 1],
-        ['pass', 2, 2, 2, 2, 2, 2, 2, 2, 2, 7, 5],
-        ['skip', 5, 6, 7, 7, 3, 3, 3, 3, 3, 0, 4]];
+    var data = {"mockdata" : [
+                    ['fail', 3, 4, 4, 6, 4, 5, 5, 5, 5, 2],
+                    ['pass', 2, 2, 2, 2, 2, 2, 2, 2, 2, 7],
+                    ['skip', 5, 6, 7, 7, 3, 3, 3, 3, 3, 0]
+                ],
+                "buildNumbers" : [7,8,9,10,11,12,13,14,15,16],
+                "buildTimes" : ["time stamp here"], //display on mousover
+                "urlBase" : "http://localhost:8080/jenkins/job/test_job_freestyle_project/"
+    };
 
     var chart = c3.generate({
         bindto: '#' + id,
         data: {
             groups:[['fail', 'pass', 'skip']],
             type: 'bar',
-            columns: mockdata,
+            columns: data.mockdata,
             colors: {
                 'fail': '#EF2929',
                 'pass': '#729FCF',
                 'skip': '#FCE94F'
             },
-            order: null   // stack order by data definition.
-            //groups: ["pass", "fail", "skip"]
+            order: null,   // stack order by data definition.
+            onclick: function (d, element) {
+                window.open(data.urlBase + d.x,"_self")
+                alert("Go to new page here!!!");
+                console.log(d);
+                console.log(element);
+            },
+            onmouseover: function (d) {
+
+            }
         },
         bar: {
             width: {
