0974755eff4019f27f2c64c18955a24bdfb4af02
Saksa <eric.saksa@ES200873ML.i3domain.inin.com>
2015-07-23 11:12:42 -0400

    adding duration to tooltip

--- src/main/webapp/js/draw_results.js
+++ src/main/webapp/js/draw_results.js
@@ -63,11 +63,38 @@ function resultsGraph(id, data) {
         tooltip: {
             format: {
                 title: function (d) {
-                    return 'Build ' + data.buildNum[d] + ": " + data.duration.concat().reverse()[d];
+                    return 'Build ' + data.buildNum[d];
                 },
-                value: function (name, id, index,value) {
+                value: function (name, id, index, value) {
                     return name;
                 }
+            },
+            contents: function (d, defaultTitleFormat, defaultValueFormat, color) {
+                var $$ = this, config = $$.config,
+                      titleFormat = config.tooltip_format_title || defaultTitleFormat,
+                      nameFormat = config.tooltip_format_name || function (name) { return name; },
+                      valueFormat = config.tooltip_format_value || defaultValueFormat,
+                      text, i, title, value, name, bgcolor;
+                for (i = 0; i < d.length; i++) {
+                    if (! (d[i] && (d[i].value || d[i].value === 0))) { continue; }
+                        if (! text) {
+                            title = titleFormat ? titleFormat(d[i].x) : d[i].x;
+                            text = "<table class='" + $$.CLASS.tooltip + "'>" + (title || title === 0 ? "<tr><th colspan='2'>" + title + "</th></tr>" : "");
+                        }
+                        name = nameFormat(d[i].name);
+                        value = valueFormat(d[i].value, d[i].ratio, d[i].id, d[i].index);
+                        bgcolor = $$.levelColor ? $$.levelColor(d[i].value) : color(d[i].id);
+
+                        text += "<tr class='" + $$.CLASS.tooltipName + "-" + d[i].id + "'>";
+                        text += "<td class='name'><span style='background-color:" + bgcolor + "'></span>" + name + "</td>";
+                        text += "<td class='value'>" + value + "</td>";
+                        text += "</tr>";
+                }
+                console.log(d);
+                text += "<tr class='" + $$.CLASS.tooltipName + "-" + "final" + "'>";
+                text += "<td class='name' colspan=2>" + "Duration: " + data.duration.concat().reverse()[d[0].index] + "</td>";
+                text += "</tr>";
+                return text + "</table>";
             }
         }
     });
