58f3979e084670dfbe142795780c965bd0cd1522
Nick Winterer <enndubyu@gmail.com>
2015-07-23 15:15:37 -0400

    label rotate

--- src/main/webapp/js/draw_results.js
+++ src/main/webapp/js/draw_results.js
@@ -67,7 +67,7 @@ function resultsGraph(id, data) {
                 window.open(url + '/' + data.buildNum[d.index],"_self");
             }
         },
-        axis: { x: { type: 'category', categories: data.buildNum}},
+        axis: { x: { type: 'category', categories: data.buildNum, tick: { rotate: Math.max(data.buildNum.map(function (s) { return s.length; })) > 2 ? 45 : 0}}},
         grid: { lines: {front: true}, x: {show: true}, y: {show: true}},
         size: {
             width: 600
