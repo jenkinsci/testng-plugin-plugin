4fe02cc1d2b029d36347b7f89b476e605865bd70
Nick Winterer <enndubyu@gmail.com>
2015-07-23 15:36:40 -0400

    label interleave

--- src/main/webapp/js/draw_results.js
+++ src/main/webapp/js/draw_results.js
@@ -43,8 +43,6 @@ function resultsGraph(id, data) {
     data.buildStatus.reverse();
     data.duration.reverse();
 
-    var labels = Math.max.apply(Math, data.buildNum.map(function (s) { return s.length})) > 2 ? data.buildNum.slice(0).map(function (val, idx) { return idx % 3 ? "" : val}) : data.buildNum.slice(0);
-
     var transformedData = [
         ['fail'].concat(data.fail.reverse()),
         ['pass'].concat(data.pass.reverse()),
@@ -69,7 +67,7 @@ function resultsGraph(id, data) {
                 window.open(url + '/' + data.buildNum[d.index],"_self");
             }
         },
-        axis: { x: { type: 'category', categories: labels}},
+        axis: { x: { type: 'category', categories: data.buildNum}},
         grid: { lines: {front: true}, x: {show: true}, y: {show: true}},
         size: {
             width: 600
