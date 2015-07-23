41a1642b1273df22e16433490b9b2f7eb724983b
Nick Winterer <enndubyu@gmail.com>
2015-07-23 09:39:05 -0400

    rename

--- src/main/resources/hudson/plugins/testng/TestNGProjectAction/index.groovy
+++ src/main/resources/hudson/plugins/testng/TestNGProjectAction/index.groovy
@@ -12,7 +12,7 @@ link(rel: "stylesheet", href:"${app.rootUrl}/plugin/testng-plugin/css/c3.min.css
 
 script(src:"${app.rootUrl}/plugin/testng-plugin/js/d3.min.js")
 script(src:"${app.rootUrl}/plugin/testng-plugin/js/c3.min.js")
-script(src:"${app.rootUrl}/plugin/testng-plugin/js/draw_viz.js")
+script(src:"${app.rootUrl}/plugin/testng-plugin/js/draw_results.js")
 
 
 l.layout(title: "TestNG Results Trend") {
