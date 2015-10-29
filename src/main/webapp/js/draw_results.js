f7fbe1ca9b63decbdbe8651552df6c88dcff3dd7
Saksa <eric.saksa@es200873ml.i3domain.inin.com>
2015-10-29 16:13:32 -0400

    make x axis labels fit on the main graph when the build number is more than 3 digits long

--- src/main/webapp/js/draw_results.js
+++ src/main/webapp/js/draw_results.js
@@ -40,6 +40,13 @@ function resultsGraph(id, data) {
     }
 
     data.buildNum.reverse();
+
+
+
+    //TODO remove after testing
+    data.buildNum = [1000,1001,1002,1003,1004,1005,1006,1007,1008,1009,1010,1011,1012,1013,1014,1015,1016,1017,1018,"",1020,"",1022,"",1024];
+
+
     data.buildStatus.reverse();
     data.duration.reverse();
 
@@ -67,7 +74,12 @@ function resultsGraph(id, data) {
                 window.open(url + '/' + data.buildNum[d.index],"_self");
             }
         },
-        axis: { x: { type: 'category', categories: data.buildNum},
+        axis: { x: { type: 'category',
+                     categories: data.buildNum,
+                     tick: {culling: {max:13},
+                            fit: false
+                           }
+                   },
                 y: {tick: {format: function (d) {return (parseInt(d) == d) ? d : null;}}}
         },
         grid: { lines: {front: true}, x: {show: true}, y: {show: true}},
